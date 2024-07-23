98c25cf819f06adb91944be769fd9087435ce1da
kytran-saritasa <ky.tran@saritasa.com>
2024-07-23 13:19:45 +0700

    Migrate standalone component and Install Angular Material JC-639

--- apps/angular/src/main.ts
+++ apps/angular/src/main.ts
@@ -1,13 +1,16 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
+import { appRoutes } from './app/app.routes';
 import { environment } from './environments/environment';
 
 if (environment.production) {
 	enableProdMode();
 }
 
-platformBrowserDynamic()
-	.bootstrapModule(AppModule)
-	.catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+	providers: [provideRouter(appRoutes), provideAnimationsAsync()],
+}).catch(err => console.error(err));
