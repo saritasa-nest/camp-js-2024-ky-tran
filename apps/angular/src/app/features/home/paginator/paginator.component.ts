e9d1069a4eaf879ee207c7284b6a30599f21d046
kytran-saritasa <ky.tran@saritasa.com>
2024-07-31 14:52:17 +0700

    Custom Paginator items per page label JC-657

--- apps/angular/src/app/features/home/paginator/paginator.component.ts
+++ apps/angular/src/app/features/home/paginator/paginator.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { booleanAttribute, ChangeDetectionStrategy, Component, Input, numberAttribute, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 /** PaginatorComponent. */
 @Component({
@@ -10,4 +10,31 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 	imports: [MatPaginatorModule],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PaginatorComponent {}
+export class PaginatorComponent implements OnInit {
+	@ViewChild(MatPaginator, { static: true }) private readonly paginator!: MatPaginator;
+
+	/** The length of the total number of items that are being paginated. */
+	@Input({ transform: numberAttribute }) protected readonly length = 100;
+
+	/** Number of items to display on a page. */
+	@Input({ transform: numberAttribute }) protected readonly pageSize = 10;
+
+	/** The set of provided page size options to display to the user. */
+	@Input() protected readonly pageSizeOptions = [5, 10, 25, 100];
+
+	/** Whether to show the first/last buttons UI to the user. */
+	@Input({ transform: booleanAttribute }) protected readonly showFirstLastButtons = true;
+
+	/** On Init. */
+	public ngOnInit(): void {
+		this.paginator._intl.itemsPerPageLabel = 'Anime per page:';
+	}
+
+	/**
+	 * Change event object that is emitted when the user selects a different page size or navigates to another page.
+	 * @param pageEvent Page event.
+	 */
+	protected onPageChange(pageEvent: PageEvent): void {
+		console.log(pageEvent);
+	}
+}
