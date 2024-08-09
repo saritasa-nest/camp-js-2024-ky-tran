3574bc0cdaae5806dfe2068d209c5799a92e04ff
kytran-saritasa <ky.tran@saritasa.com>
2024-08-09 11:57:14 +0700

    Pagination mapper namespace JC-657

--- apps/angular/src/core/services/anime.service.ts
+++ apps/angular/src/core/services/anime.service.ts
@@ -5,10 +5,10 @@ import { catchError, map, Observable, shareReplay, throwError } from 'rxjs';
 import { UrlConfig } from '@js-camp/angular/config/url.config';
 import { Pagination } from '@js-camp/core/models/pagination.model';
 import { PaginationDto } from '@js-camp/core/dtos/pagination.dto';
-import { PaginationMapper } from '@js-camp/angular/core/mappers/pagination.mapper';
 import { Anime } from '@js-camp/core/models/anime.model';
 import { AnimeDto } from '@js-camp/core/dtos/anime.dto';
 import { AnimeMapper } from '@js-camp/core/mappers/anime.mapper';
+import { PaginationMapper } from '@js-camp/core/mappers/pagination.mapper';
 
 /** Anime service. */
 @Injectable({ providedIn: 'root' })
@@ -17,8 +17,6 @@ export class AnimeService {
 
 	private readonly urlConfig = inject(UrlConfig);
 
-	private readonly paginationMapper = inject(PaginationMapper);
-
 	/**
 	 * Get anime list.
 	 * @param httpParams HttpParams.
@@ -26,7 +24,7 @@ export class AnimeService {
 	public getAnimeList(httpParams: HttpParams): Observable<Pagination<Anime>> {
 		return this.httpClient.get<PaginationDto<AnimeDto>>(this.urlConfig.animeUrl, { params: httpParams })
 			.pipe(
-				map(responseDto => this.paginationMapper.fromDto(responseDto, AnimeMapper.fromDto)),
+				map(responseDto => PaginationMapper.fromDto(responseDto, AnimeMapper.fromDto)),
 				shareReplay({ refCount: true, bufferSize: 1 }),
 				catchError((_: unknown) => throwError(() => new Error('Something went wrong. Please try again.'))),
 			);
