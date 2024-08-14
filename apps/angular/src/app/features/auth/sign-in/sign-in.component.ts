23d119a8d44e7af950bc90b8d9c438f34227f459
kytran-saritasa <ky.tran@saritasa.com>
2024-08-14 16:06:52 +0700

    Implement sign in form with form builder JC-676

--- apps/angular/src/app/features/auth/sign-in/sign-in.component.ts
+++ apps/angular/src/app/features/auth/sign-in/sign-in.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { AbstractControl, NonNullableFormBuilder, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 
 /** Sign In component. */
 @Component({
@@ -8,12 +9,29 @@ import { MatIconModule } from '@angular/material/icon';
 	standalone: true,
 	templateUrl: './sign-in.component.html',
 	styleUrl: './sign-in.component.css',
-	imports: [RouterModule, MatIconModule],
+	imports: [RouterModule, MatIconModule, ReactiveFormsModule],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SignInComponent {
+	private readonly formBuilder = inject(NonNullableFormBuilder);
+
+	/** Sign in form group. */
+	protected readonly form = this.formBuilder.group({
+		email: ['', [Validators.email, Validators.required]],
+		password: ['', [Validators.required, Validators.minLength(8), this.validateNonNumericPassword]],
+	});
+
 	/** Hide password signal. */
 	protected readonly hidePassword = signal(true);
+
+	private validateNonNumericPassword(control: AbstractControl): ValidationErrors | null {
+		if (!control.value) {
+			return null;
+		}
+
+		const isNumeric = /^\d+$/.test(control.value);
+		return isNumeric ? { numericPassword: true } : null;
+	}
 
 	/**
 	 * Toggle hide password event.
@@ -21,5 +39,11 @@ export class SignInComponent {
 	 */
 	protected onToggleHidePassword(): void {
 		this.hidePassword.set(!this.hidePassword());
+	}
+
+	/** On submit. */
+	protected onSubmit(): void {
+		console.log('Is form valid -->', this.form.valid);
+		console.log(this.form);
 	}
 }
