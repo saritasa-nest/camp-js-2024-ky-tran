70353a5a5916501acfb29472db145321dc03ec54
kytran-saritasa <ky.tran@saritasa.com>
2024-08-16 14:56:10 +0700

    Toggle sidebar JC-676

--- apps/angular/src/shared/components/main-layout/main-layout.component.ts
+++ apps/angular/src/shared/components/main-layout/main-layout.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 import { ProgressSpinnerComponent } from '@js-camp/angular/shared/components/progress-spinner/progress-spinner.component';
 import { ErrorMessageComponent } from '@js-camp/angular/shared/components/error-message/error-message.component';
 import { UserService } from '@js-camp/angular/core/services/user.service';
@@ -11,9 +11,10 @@ import { UserService } from '@js-camp/angular/core/services/user.service';
 	standalone: true,
 	templateUrl: './main-layout.component.html',
 	styleUrl: './main-layout.component.css',
-	imports: [RouterModule, ProgressSpinnerComponent, ErrorMessageComponent],
+	imports: [CommonModule, RouterModule, ProgressSpinnerComponent, ErrorMessageComponent],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainLayoutComponent {
-	private readonly userService = inject(UserService);
+	/** User service. */
+	protected readonly userService = inject(UserService);
 }
